6124043474bb18d9a244b01c45d60adef01065f7
Jose Vasquez <joseevasquezbarboza@gmail.com>
2024-10-22 21:01:24 +0200

    added footer

--- src/main/java/es/dws/classProject/controller/HomeController.java
+++ src/main/java/es/dws/classProject/controller/HomeController.java
@@ -1,6 +1,9 @@
 package es.dws.classProject.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping({ "/", "/index" })
-    public String showHome() {
+    public String showHome(Model model) {
+        model.addAttribute("currentYear", LocalDate.now().getYear());
         return "indexView";
     }
 
